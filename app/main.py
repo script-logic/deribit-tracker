adaf5fd3e2fbe9174387a9666d6e880b8b224a3d
script-logic <dev.scriptlogic@gmail.com>
2026-01-22 19:51:53 +0300

    ci: fix gitlab ci

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://127.0.0.1:8000"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"],  # TODO: get
     allow_headers=["*"],
 )
 
